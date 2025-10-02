e73333482d2e1a0951f5a85a29c150c2550e941d
AA-ron Henderson <ahenderson@g2.com>
2025-10-02 05:38:45 -0600

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,41 +1,45 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
-#
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
-# should only set this value when you want to run 2 or more workers. The
-# default is already 1.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+# Processes count, allows better CPU utilization when executing Ruby code.
+workers(ENV.fetch("WEB_CONCURRENCY") { 2 })
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Thread per process count allows context switching on IO-bound tasks for better CPU utilization.
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
+threads(threads_count, threads_count)
 
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+# Reduce memory usage on copy-on-write (CoW) systems.
+preload_app!
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+# Ruby buildpack sets RAILS_ENV and RACK_ENV in production.
+run_env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
+environment(run_env)
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+# Support IPv6 by binding to host `::` in production instead of `0.0.0.0` and `::1` instead of `127.0.0.1` in development.
+host = run_env == "production" ? "::" : "::1"
+
+# PORT environment variable is set by Heroku in production.
+port(ENV.fetch("PORT") { 3000 }, host)
+
+# Allow Puma to be restarted by the `rails restart` command locally.
+plugin(:tmp_restart)
+
+# Heroku strongly recommends upgrading to Puma 7+. If you cannot upgrade,
+# please see the Puma 6 and prior configuration section below.
+#
+# Puma 7+ already supports PUMA_PERSISTENT_TIMEOUT natively. Older Puma versions set:
+#
+# ```
+# persistent_timeout(ENV.fetch("PUMA_PERSISTENT_TIMEOUT") { 95 }.to_i)
+# ```
+#
+# Puma 7+ fixes a keepalive issue that affects long tail response time with Router 2.0.
+# Older Puma versions set:
+#
+# ```
+# enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+# ```
+# Processes count, allows better CPU utilization when executing Ruby code.
+workers(ENV.fetch("WEB_CONCURRENCY") { 2 })
+# Thread per process count allows context switching on IO-bound tasks for better CPU utilization.
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
+threads(threads_count, threads_count)
+# Reduce memory usage on copy-on-write (CoW) systems.
+preload_app!
