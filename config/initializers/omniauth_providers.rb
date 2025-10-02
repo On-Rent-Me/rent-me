d38404a4b7ee4eba0c94dad8069d090ebf8932f9
AA-ron Henderson <ahenderson@g2.com>
2025-10-02 10:01:50 -0600

    Support Google OAuth via environment variables for Heroku

--- config/initializers/omniauth_providers.rb
+++ config/initializers/omniauth_providers.rb
@@ -1,4 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development? || Rails.env.test?
-  provider :google_oauth2, Rails.application.credentials.dig(:oauth, :google, :client_id), Rails.application.credentials.dig(:oauth, :google, :client_secret)
+
+  # Use environment variables in production (Heroku), fall back to credentials for local dev
+  google_client_id = ENV['GOOGLE_CLIENT_ID'] || Rails.application.credentials.dig(:oauth, :google, :client_id)
+  google_client_secret = ENV['GOOGLE_CLIENT_SECRET'] || Rails.application.credentials.dig(:oauth, :google, :client_secret)
+
+  provider :google_oauth2, google_client_id, google_client_secret
 end
