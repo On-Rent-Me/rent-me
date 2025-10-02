ed05b229fcff17d82c9f8eddac8dc74acc06c995
AA-ron Henderson <ahenderson@g2.com>
2025-10-02 09:32:49 -0600

    move to postgres

--- config/puma.rb
+++ config/puma.rb
@@ -1,16 +1,18 @@
-# Processes count, allows better CPU utilization when executing Ruby code.
-workers(ENV.fetch("WEB_CONCURRENCY") { 2 })
+# Ruby buildpack sets RAILS_ENV and RACK_ENV in production.
+run_env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
+environment(run_env)
 
 # Thread per process count allows context switching on IO-bound tasks for better CPU utilization.
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
 threads(threads_count, threads_count)
 
-# Reduce memory usage on copy-on-write (CoW) systems.
-preload_app!
+# Processes count, allows better CPU utilization when executing Ruby code.
+# Set to 0 for development on macOS to avoid fork issues with objc runtime
+worker_count = run_env == "development" ? 0 : ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers(worker_count)
 
-# Ruby buildpack sets RAILS_ENV and RACK_ENV in production.
-run_env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
-environment(run_env)
+# Reduce memory usage on copy-on-write (CoW) systems.
+preload_app! if worker_count > 0
 
 # Support IPv6 by binding to host `::` in production instead of `0.0.0.0` and `::1` instead of `127.0.0.1` in development.
 host = run_env == "production" ? "::" : "::1"
@@ -36,10 +38,3 @@ plugin(:tmp_restart)
 # ```
 # enable_keep_alives(false) if respond_to?(:enable_keep_alives)
 # ```
-# Processes count, allows better CPU utilization when executing Ruby code.
-workers(ENV.fetch("WEB_CONCURRENCY") { 2 })
-# Thread per process count allows context switching on IO-bound tasks for better CPU utilization.
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
-threads(threads_count, threads_count)
-# Reduce memory usage on copy-on-write (CoW) systems.
-preload_app!
